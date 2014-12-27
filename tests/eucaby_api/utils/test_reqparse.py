0594095022b6e0221a1b231249dc6c78d33836e5
Alex Dementsov <dexity@gmail.com>
2014-12-27 09:47:09 -0800

    Implemented test_reqparse.py

--- tests/eucaby_api/utils/test_reqparse.py
+++ tests/eucaby_api/utils/test_reqparse.py
@@ -1,8 +1,43 @@
 import unittest
+from eucaby_api.utils import reqparse
+from eucaby_api import wsgi
 
 class RequestParserTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.app = wsgi.create_app()
+        self.app.config.from_object('eucaby_api.config.Testing')
+
+    def test_parse_args(self):
+        arg1 = reqparse.Argument('param1')
+        arg2 = reqparse.Argument('param2', required=True)
+        parser = reqparse.RequestParser()
+        parser.add_argument(arg1)
+        parser.add_argument(arg2)
+        with self.app.test_request_context(
+                '?param1=test1&param2=test2&param3=test3'):
+            parsed_args = dict(param1='test1', param2='test2')
+            # Non-strict
+            args = parser.parse_args()
+            self.assertEqual(parsed_args, args)
+            # Strict
+            self.assertRaises(reqparse.InvalidError, parser.parse_args,
+                              strict=True)
+            try:
+                parser.parse_args(strict=True)
+            except reqparse.InvalidError as e:
+                self.assertEqual({}, e.errors)
+                self.assertEqual(parsed_args, e.namespace)
+                self.assertEqual(
+                    dict(param3='Unrecognized parameter'), e.unparsed)
 
 
+class ArgumentParserTest(unittest.TestCase):
+
+    def test_handle_validation_error(self):
+        arg = reqparse.Argument('some_param')
+        self.assertRaises(ValueError, arg.handle_validation_error, 'some_error')
+
+
+if __name__ == '__main__':
+    unittest.main()
