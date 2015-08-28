2f01c8db1371ea1bb1d34a028dd758a158551105
Alex Dementsov <dexity@gmail.com>
2015-08-28 12:08:04 -0700

    Updated configuration files for mobile

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,4 +3,4 @@
 gcloud preview app deploy \
     app.yaml api.yaml push.yaml mail.yaml \
     index.yaml dispatch.yaml queue.yaml cron.yaml \
-    --project eucaby-dev --version=1
+    --project eucaby-dev --version=1 --set-default
